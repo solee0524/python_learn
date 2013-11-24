fab0266e099efe17148802ed2d4dd0669844006c
solee0524 <iblee0524@gmail.com>
2013-11-24 17:05:27 +0800

    The use of functions

---
+++ course/Functions.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+
+# This is course of Pyhton on Coursera (Week 1)
+def triangle_area(base, height):
+    area = (1.0 / 2)* base * height
+    return area
+
+print "The triangle_area is", triangle_area(1, 3)
+
